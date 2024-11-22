483ce2f800940e3bcbccc5bb2c4ea94b2e640d42
Bruno Cicerchia <brunocicerchia.dev@gmail.com>
2024-11-22 08:25:14 -0300

    Chill

--- EntradaFactory.java
+++
@@ -1,6 +0,0 @@
-public class EntradaFactory {
-    public static Entrada crearEntrada(Funcion funcion, Ubicacion ubicacion) {
-        double precio = ubicacion.calcularCostoUbicacion();
-        return new Entrada(precio);
-    }
-}
